3e11f2f0a66147ea4b186cba8a29dae6b37fef6f
AjmalCholassery <ajmalch@gmail.com>
2020-09-23 21:46:40 -0400

    Security oidc(resource server) improvements

--- src/main/java/com/example/jpaaudit/configuartion/OidcSecurityConfig.java
+++ src/main/java/com/example/jpaaudit/configuartion/OidcSecurityConfig.java
@@ -1,10 +1,19 @@
 package com.example.jpaaudit.configuartion;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.Collection;
 
 @Configuration
 @ConditionalOnProperty(value = "auth.mode", havingValue = "oidc")
@@ -15,8 +24,31 @@ public class OidcSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests(authRequest -> authRequest.anyRequest().authenticated())
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer(oauth2->
+                        oauth2.jwt(Customizer.withDefaults()));
 
+
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter(){
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new CustomJwtGrantedAuthoritiesConverter());
+        return converter;
+    }
+
+    public static class CustomJwtGrantedAuthoritiesConverter implements
+            Converter<Jwt, Collection<GrantedAuthority>>{
+
+        final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        //TODO At the moment we are not doing anything specific.
+        // But, this can be used to add authorities to user
+        @Override
+        public Collection<GrantedAuthority> convert(Jwt jwt) {
+            return jwtGrantedAuthoritiesConverter.convert(jwt);
+
+        }
     }
 
 }
