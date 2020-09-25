a8c6e861414b1b7dd1f48ab51833fcbee6484255
AjmalCholassery <ajmalch@gmail.com>
2020-09-24 22:14:21 -0400

    Swagger and swagger ui initial implementation

--- src/main/java/com/example/JpaauditApplication.java
+++ src/main/java/com/example/JpaauditApplication.java
@@ -3,17 +3,14 @@ package com.example;
 import com.example.jpaaudit.model.Customer;
 import com.example.jpaaudit.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.ApplicationScope;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -46,7 +43,7 @@ public class JpaauditApplication {
 
     }
 
-    @GetMapping("clients")
+    @GetMapping("/clients")
     public Customer getCustomer() {
         return customerRepository.findById(1L)
                 .orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
