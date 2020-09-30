f11b40ca3da993211902a87aadf6b147312fa3c4
AjmalCholassery <ajmalch@gmail.com>
2020-09-29 23:36:26 -0400

    Unit tests with 100% coverage on CustomerController

--- src/test/java/com/example/controller/CustomerControllerTest.java
+++ src/test/java/com/example/controller/CustomerControllerTest.java
@@ -2,15 +2,17 @@ package com.example.controller;
 
 import com.example.jpaaudit.model.Customer;
 import com.example.jpaaudit.repository.CustomerRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,21 +24,36 @@ class CustomerControllerTest {
     @InjectMocks
     private CustomerController customerController;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void getCustomerByFirstNameShouldCallCustomerRepository() {
+
         when(customerRepository.findCustomerByFirstName(anyString()))
                 .thenReturn(Optional.of(new Customer("ajmal"
                         , "Ajmal"
                         , "Cholassery"
                         , Customer.Gender.MALE)));
-    }
 
-    @Test
-    void getCustomerByFirstNameShouldCallCustomerRepository() {
-        final var ajmal = customerController.getCustomerByFirstName("ajmal");
-
+        customerController.getCustomerByFirstName("ajmal");
         verify(customerRepository, times(1))
                 .findCustomerByFirstName(eq("ajmal"));
 
     }
+
+    @Test
+    void getCustomerByFirstNameShouldThrowResourceNotFoundException() {
+
+        when(customerRepository.findCustomerByFirstName(anyString()))
+                .thenReturn(Optional.empty());
+
+        final var resourceNotFoundException = assertThrows(ResourceNotFoundException.class,
+                () -> customerController.getCustomerByFirstName("ajmal"));
+
+        assertEquals(
+                "Customer with firstname ajmal not found"
+                , resourceNotFoundException.getMessage()
+        );
+
+    }
+
+
 }
