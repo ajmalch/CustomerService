5b550b4c73199d5bb257ee8be25112a8f26988fa
Ajmal Cholassery <ajmalch@gmail.com>
2024-01-15 19:28:52 -0500

    fix codeql-analysis.yml

--- src/main/java/com/example/controller/CustomerController.java
+++ src/main/java/com/example/controller/CustomerController.java
@@ -6,15 +6,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(path = "/customers")
 public class CustomerController {
 
     private final CustomerRepository customerRepository;
 
-    @GetMapping("/customer/{firstName}")
+    @GetMapping("/{firstName}")
     public Customer getCustomerByFirstName(@PathVariable String firstName) {
 
         return customerRepository.findCustomerByFirstName(firstName)
