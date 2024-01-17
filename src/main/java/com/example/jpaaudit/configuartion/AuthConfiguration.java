1d256b6eb02cc4574300750008e4f2c9b2a557a2
Ajmal Cholassery <ajmalch@gmail.com>
2024-01-16 22:51:06 -0500

    migrate from maven to gradle and upgrade to java 21 and spring boot 3 (#32)
    * Maven to Gradle migration
    Upgrade top java 21 and spring boot 3.2.1
    * Adding gradlew

--- src/main/java/com/example/jpaaudit/configuartion/AuthConfiguration.java
+++ src/main/java/com/example/jpaaudit/configuartion/AuthConfiguration.java
@@ -1,13 +1,12 @@
 package com.example.jpaaudit.configuartion;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
 @Configuration
 @EnableConfigurationProperties
